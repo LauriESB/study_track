1edcac2364e24ba21ee6246143e782c55c0c7df6
Lauri Santinelli <ich.lauri@gmail.com>
2023-09-26 23:11:59 -0300

    UpperCase upload

---
+++ CodeWars/MakeUpperCase/src/main/java/com/mycompany/makeuppercase/MakeUpperCase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.makeuppercase;
+
+/**
+ *
+ * @author lwuc
+ */
+public class MakeUpperCase {
+  public static String makeUpperCase(String str){
+    return str == null ? null : str.toUpperCase();
+  }
+}
