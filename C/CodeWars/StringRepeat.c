93b2d5cabbe72e276e91c2bc81a46ec39af5d6d8
Lauri Santinelli <ich.lauri@gmail.com>
2023-10-07 21:20:19 -0300

    string repeat exercise

---
+++ C/CodeWars/StringRepeat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+char *repeat_str(size_t count, const char *src)
+{
+// allocate a string on the heap
+  return calloc(1, 1);
+}
+
+int main(){
+
+
+}
